203078677399d904ce2262a4cac365be9711f5cb
Arditti93 <alex.arditti@hotmail.com>
2023-04-25 16:19:57 +0100

    Login working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Login from './componets/Login'
+import Login from './components/Login'
 
+import {useState } from 'react'
 
 const App = () => {
+
+  const [user, setUser] = useState()
+
   return (
     <div>
-      <Login />
+      <Login newUser={setUser} />
+      {user 
+        ?
+        <h2>Hello welcome {user} you have logged in</h2>
+        :
+        <h2>Please log in</h2>
+      }
     </div>
   );
 };
